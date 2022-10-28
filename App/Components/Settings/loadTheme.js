446264ac414bf5c203c78f27acc74aa012e4377d
Nebula <74029703+Nebula-mx@users.noreply.github.com>
2022-10-28 11:55:51 -0500

    Bug fixes after 0.9.2

--- App/Components/Settings/loadTheme.js
+++ App/Components/Settings/loadTheme.js
@@ -47,6 +47,7 @@ export function loadTheme() {
             --settings-menu_link: #5f0d84;
             --settings-menu-light-list: #EEE;
             --settings-menu-light-list-items: #fff;
+            --settings-menu-lignt-items-hoverBg: ;
             --settings-menu-light-content: #fff;
             --settings-menu-light-options:#EEE;
             --settings-menu-light-selects:#D9D9D9;
@@ -88,7 +89,7 @@ export function loadTheme() {
             --settings-menu_link: #ff98d2;
             --settings-menu-light-list: #070707;
             --settings-menu-light-list-items: #0e0e0e;
-            --settings-menu-lignt-list-items-hoverBg: #111;
+            --settings-menu-lignt-items-hoverBg: #222;
             --settings-menu-light-items: #0E0E0E; 
             --settings-menu-light-content: #000;
             --settings-menu-light-options: #0E0E0E;
