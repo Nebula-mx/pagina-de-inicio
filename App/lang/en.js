05e6d9f3a04c2d0774a431bed4beda5e4b1f9361
fabrisdev <ezepuntocom@hotmail.com>
2023-05-08 20:41:44 -0300

    Languages system

---
+++ App/lang/en.js
@@ -0,0 +1,73 @@
+export default {
+    "commonWords": {
+        "default": "Default",
+        "clean": "Clean"
+    },
+    "settings": {
+        "title": "Settings",
+        "general": {
+            "title": "General",
+            "categories": {
+                "general": "General",
+                "extra": "Extra",
+                "resetAppValues": "Reset app values",
+            },
+            "shortcutsLimit": {
+                "legend": "Change shortcuts limit",
+                "p": "This feature requires to reload this page."
+            },
+            "searchEngine": {
+                "legend": "Select search engine",
+                "p": "Change the default search engine."
+            },
+            "searchInNewTab": {
+                "legend": "Open search on NewTab",
+                "p": "Make every search you make open in new tabs."
+            },
+            "weatherCity": {
+                "legend": "Set weather city",
+                "p": "Change your preferred city to display the weather.",
+                "manualSetButton": "Manual set",
+                "autoSetButton": "Auto set"
+            },
+            "appLanguage": {
+                "legend": "Change app language",
+                "p": "Choose your preferred language to use."
+            },
+            "exportSettings": {
+                "legend": "Export settings",
+                "p": "Export your settings to use them in another browser.",
+                "button": "Export"
+            },
+            "exportShortcuts": {
+                "legend": "Export shortcuts",
+                "p": "Export all your shortcuts.",
+                "button": "Export"
+            },
+            "importSettings": {
+                "legend": "Import settings",
+                "p": "Import your custom settings to use.",
+                "button": "Import"
+            },
+            "importShortcuts": {
+                "legend": "Import shortcuts",
+                "p": "Import shortcuts from other place.",
+                "button":  "Import"
+            },
+            "resetSettings": {
+                "legend": "Reset settings",
+                "button": "Reset"
+            },
+            "deleteShortcuts": {
+                "legend": "Delete all shortcuts",
+                "button": "Delete all"
+            }
+        },
+        "appearance": {
+
+        },
+        "about": {
+
+        }
+    }
+}
---
+++ App/lang/es.js
@@ -0,0 +1,73 @@
+export default {
+    "commonWords": {
+        "default": "Por defecto",
+        "clean": "Limpio"
+    },
+    "settings": {
+        "title": "Ajustes",
+        "general": {
+            "title": "General",
+            "categories": {
+                "general": "General",
+                "extra": "Extra",
+                "resetAppValues": "Reestablecer valores",
+            },
+            "shortcutsLimit": {
+                "legend": "Cambiar límite de atajos",
+                "p": "Esta función requiere recargar la página."
+            },
+            "searchEngine": {
+                "legend": "Seleccionar motor de búsqueda",
+                "p": "Cambia el motor de búsqueda."
+            },
+            "searchInNewTab": {
+                "legend": "Abrir búsqueda en una nueva pestaña",
+                "p": "Haz que cada búsqueda que hagas se abra en una nueva pestaña."
+            },
+            "weatherCity": {
+                "legend": "Configurar ciudad del clima",
+                "p": "Cambia a la ciudad que prefieras saber el clima.",
+                "manualSetButton": "Ajustar manualmente",
+                "autoSetButton": "Ajustar automáticamente"
+            },
+            "appLanguage": {
+                "legend": "Cambiar idioma de la aplicación",
+                "p": "Elige el idioma que prefieras utilizar."
+            },
+            "exportSettings": {
+                "legend": "Exportar ajustes",
+                "p": "Exporta tus ajustes para usarlos en otro navegador.",
+                "button": "Exportar"
+            },
+            "exportShortcuts": {
+                "legend": "Exportar atajos",
+                "p": "Exporta todos tus atajos.",
+                "button": "Exportar"
+            },
+            "importSettings": {
+                "legend": "Importar ajustes",
+                "p": "Importa tus ajustes especiales para utilizarlos.",
+                "button": "Importar"
+            },
+            "importShortcuts": {
+                "legend": "Importar atajos",
+                "p": "Importa atajos de otro lugar.",
+                "button":  "Importar"
+            },
+            "resetSettings": {
+                "legend": "Reestablecer ajustes",
+                "button": "Reestablecer"
+            },
+            "deleteShortcuts": {
+                "legend": "Borrar todos los atajos",
+                "button": "Borrar todos"
+            }
+        },
+        "appearance": {
+
+        },
+        "about": {
+
+        }
+    }
+}
